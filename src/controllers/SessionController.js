0c536d4dcb7fce57f60ed4899f56cd599f702c84
Elvis Felix <elvisjaspion@gmail.com>
2023-02-22 18:56:57 -0300

    Update SessionController.js

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -1,15 +1,27 @@
-const User = require('../models/User')
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
 
 module.exports = {
-    async store(req, res) {
-        const { email } = req.body
+  async store(req, res) {
+    const { email, password } = req.body;
 
-        let user = await User.findOne({ email })
-
-        if (!user) {
-            user = await User.create({ email })
-        }
-
-        return res.json(user)
+    if (!email || !password) {
+      return res.status(400).json({ message: 'Email and password are required' });
     }
-}
+
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      user = await User.create({ email, password: hashedPassword });
+    } else {
+      const passwordMatch = await bcrypt.compare(password, user.password);
+
+      if (!passwordMatch) {
+        return res.status(401).json({ message: 'Invalid password' });
+      }
+    }
+
+    return res.json(user);
+  },
+};
